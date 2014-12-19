d5a522159d6d61090cddfaaad13de2be4630a787
Matt Thompson <thompsonson@gmail.com>
2014-12-19 07:28:18 +0000

    database dropdown

---
+++ app/js/db.js
@@ -0,0 +1,38 @@
+/* Main javascript  */
+
+$(function(){
+
+    // Load native UI library
+    var gui = require('nw.gui');
+
+    //instantiate markgiu app and bind it to DOM
+    var markGiuApp = new markgiu.DBGui();
+    ko.applyBindings(markGiuApp, $("#wrap")[0]);
+
+    win = gui.Window.get();
+    var nativeMenuBar = new gui.Menu({ type: "menubar" });
+    try {
+        nativeMenuBar.createMacBuiltin("My App");
+        win.menu = nativeMenuBar;
+    } catch (ex) {
+        _log.error(ex.message);
+    }
+
+/*    
+
+    //set up keyboard shortcuts
+    document.addEventListener('keyup', function (e) {
+		if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
+            markGiuApp.chooseFileToOpen();
+		} else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
+            //markGiuApp.saveFile(); // not used as now auto saving...
+            $('#savefileas').click();
+		} else if (e.keyCode == 'N'.charCodeAt(0) && e.ctrlKey) {
+            markGiuApp.addTabNew();
+        }
+	});
+*/
+    //show window
+    gui.Window.get().show();
+    
+});
